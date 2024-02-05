445f3457e029911f1c27f7cda2e8b186646dc948
santosh sewlal <santosh.sewlal@gmail.com>
2024-02-06 01:05:46 +0200

    Update setup.py

--- dagster-dbt/setup.py
+++ dagster-dbt/setup.py
@@ -10,6 +10,10 @@ setup(
         "dbt-core==1.4.7",
         "dbt-duckdb==1.4.1",
         "dagster-duckdb==0.20.10",
+        "pyarrow",
+        "numpy",
+        "plotly",
+        "pandas",
         "pendulum<3.0"                                      #  https://github.com/dagster-io/dagster/discussions/18783#discussioncomment-7881162
     ],
     extras_require={"dev": ["dagit", "pytest"]},
